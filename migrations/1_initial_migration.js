97e78ff75e42a1d904efda3945bfefe74da780ff
curIbt <94638330+curIbt@users.noreply.github.com>
2023-04-04 21:31:50 +0000

    Update copyright date

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
-  dep
+  deployer.deploy(Migrations);
+};
---
+++ migrations/3_deploy_contracts.js
@@ -0,0 +1,6 @@
+
+var gup = artifacts.require("./GUP.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(gup);
+};
